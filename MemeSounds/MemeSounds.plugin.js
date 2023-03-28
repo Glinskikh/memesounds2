42fa6c1e78497b8182d9d9fa5a65bfcaca978963
Glinskikh <128730600+Glinskikh@users.noreply.github.com>
2023-03-28 21:57:09 +0500

    Update MemeSounds.plugin.js

--- MemeSounds/MemeSounds.plugin.js
+++ MemeSounds/MemeSounds.plugin.js
@@ -46,7 +46,8 @@ module.exports = (() => {
 				{re: /cum/gmi, file: "Vergil (1).mp3", duration: 100},
 				{re: /danteh/gmi, file: "vergil-danteh.mp3", duration: 100},
 				{re: /uu?gh/gmi, file: "vergil-uuuuuughhhh-dt.mp3", duration: 100},
-				{re: /bury the light/gmi, file: "Bury the Light.mp3", duration: 100}
+				{re: /bury the light/gmi, file: "Bury the Light.mp3", duration: 100},
+				{re: /poya/gmi, file: "kirby-poyo.mp3", duration: 100}
 			];
 
 			/* Double message event fix */
